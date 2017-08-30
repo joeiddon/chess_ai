04229459e395ced80c2e03b47566e1864ce2ee36
roadkillcat <joedominiciddon@gmail.com>
2017-08-30 15:04:08 +0100

    neatened, UI

--- basicFuncs.js
+++ basicFuncs.js
@@ -1,3 +1,18 @@
+function appendToLog(move, state){
+	//piece = state.board[move[1][0]][move[1][1]].toUpperCase()
+	document.getElementById("log").innerText += "\n" + notation(move[0][0], move[0][1]) + "-" + notation(move[1][0], move[1][1])
+}
+
+function updateInfo(depth){
+	document.getElementById("info").innerText = 
+	"WARNING:\nbuttons will lag if AI\n\n" +
+	"to play:\n" + (currentState.toPlay == "w" ? "white" : "black") + 
+	"\n\nthought for:\n" + (new Date() - start).toString() + 
+	" ms\n\nthought to depth:\n" + depth + 
+	"\n\nwhite eval.:\n" + (Math.round(evaluate(currentState, "w")*10)/10).toString() +
+	"\n\nautoplay | AI\n" + (autoplay ? "true      | " : "false    | " ) + AI.toString() + "\n\n\n"
+}
+
 function time(exp, precision){
 	start = new Date()
 	for (var i = 0; i < precision; i++){
@@ -22,7 +37,6 @@ function isUpperCase(c){
 	return c == c.toUpperCase()
 }
 
-
 function drawArrow(x1, y1, x2, y2, theta, l){
 	ctx.strokeStyle = "rgba(255,0,0,0.6)"
 	ctx.fillStyle = ctx.strokeStyle
@@ -41,6 +55,44 @@ function drawArrow(x1, y1, x2, y2, theta, l){
 	ctx.lineTo(x2, y2)
 	ctx.lineTo(x2 - l * Math.sin(beta*(Math.PI/180)), y2 - l * Math.cos(beta*(Math.PI/180)))
 	ctx.fill()
+}
+
+function drawState(state){
+	ctx.clearRect(0,0,width,width)
+	drawWoodBackground()
+	for (var r = 0; r < 8; r++){
+		for (var c = 0; c < 8; c++){
+			if (state.board[r][c] != " "){
+				ctx.drawImage(document.getElementById(state.board[r][c]), c * sqrSize, r * sqrSize, sqrSize, sqrSize)
+			}
+		}
+	}
+}
+
+function drawWoodBackground(){
+	ctx.font = (width/50).toString()+ "px monospace"
+	ctx.textAlign = "start"
+	ctx.fillStyle = "white"
+	ctx.fillRect(0,0,width,width)
+	for (var r = 0; r < 8; r++){
+		for (var c = 0; c < 8; c++){
+			ctx.fillStyle = (r+c) % 2 == 0 ? lightColor : darkColor
+			ctx.fillRect(c*sqrSize, r*sqrSize, sqrSize, sqrSize)
+			if (r == 7){
+				ctx.fillStyle = "black"
+				ctx.fillText(String.fromCharCode(97 + c), c * sqrSize + 5, width - 5)
+			} if (c == 0){
+				ctx.fillStyle = "black"
+				ctx.fillText(8 - r, 5, r * sqrSize + sqrSize + (r == 7 ? -18 : -5))
+			} if (r == 0){
+				ctx.fillStyle = "black"
+				ctx.fillText(String.fromCharCode(97 + c), c * sqrSize + 5, 18)
+			} if (c == 7){
+				ctx.fillStyle = "black"
+				ctx.fillText(8 - r, width - 18, r * sqrSize + sqrSize -5)
+			}
+		}
+	}
 }
 
 function unpackFen(fen){
@@ -74,5 +126,23 @@ function checkmateOrStalemate(state, moves, side){
 			ctx.fillText("stalemate", width/2, width/2)
 		}
 		gameover = true
+	}
+}
+
+function drawMovesArrows(moves){
+	for (var m = 0; m < moves.length; m++){
+		drawArrow((moves[m][0][1] + 0.5) * sqrSize, (moves[m][0][0] + 0.5) * sqrSize,
+		(moves[m][1][1] + 0.5) * sqrSize, (moves[m][1][0] + 0.5) * sqrSize,	35, 30)
+	}
+}
+
+function drawMove(move){
+	drawArrow((move[0][1] + 0.5) * sqrSize, (move[0][0] + 0.5) * sqrSize, (move[1][1] + 0.5) * sqrSize, (move[1][0] + 0.5) * sqrSize, 35, 30)
+}
+
+function drawMovesSqrs(moves){
+	for (var m = 0; m < moves.length; m++){
+		ctx.fillStyle = moveColor
+		ctx.fillRect(moves[m][1][1] * sqrSize, moves[m][1][0] * sqrSize, sqrSize, sqrSize)
 	}
 }
--- script.js
+++ script.js
@@ -20,8 +20,8 @@ var fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 var interstingState = { "board": [ [ " ", " ", " ", " ", " ", " ", " ", "r" ], [ " ", "b", " ", "k", " ", "p", "p", "p" ], [ "r", "b", "p", " ", "n", " ", " ", " " ], [ " ", "p", " ", "p", "P", " ", " ", " " ], [ "P", "P", " ", " ", " ", "P", " ", " " ], [ " ", " ", "N", "R", " ", " ", "P", " " ], [ " ", " ", " ", " ", " ", "N", "B", " " ], [ "R", " ", " ", " ", " ", " ", "K", " " ] ], "toPlay": "w", "castling": "undefinedundefined", "enPassant": "-", "halfmoves": 0 }
 
 var currentState = unpackFen(fen)
-var AI = true
-var autoplay = true
+var AI = false
+var autoplay = false
 var timeLimit = 2000
 var arrows = false
 var moveOptions = []
@@ -44,31 +44,27 @@ function startUp(){
 	ctx.fillText("click anywhere to load in the pieces", width/2, width/2)
 	
 	if (autoplay){
-		id = setInterval(AIMove, 200)
+		id = setInterval(AIMove, 100)//button also needs updatining if changed
 		return
 	}
 	
 	document.addEventListener("click", click)
-	//document.addEventListener("mousemove", mouse)
-}
-
-function appendToLog(move, state){
-	//piece = state.board[move[1][0]][move[1][1]].toUpperCase()
-	document.getElementById("log").innerText += "\n" + notation(move[0][0], move[0][1]) + "-" + notation(move[1][0], move[1][1])
+	
+	start = new Date() // required for move timing
+	updateInfo(0)
 }
 
 function userMove(move){
 	currentState = makeMove(currentState, move)
 	currentState.toPlay = currentState.toPlay == "w" ? "b" : "w"
+	moves = availableMoves(currentState, currentState.toPlay)
 	
 	appendToLog(move, currentState) //matters where this goes as if before state change then code vil change...
+	updateInfo(0)
 	
-	moves = availableMoves(currentState, currentState.toPlay)
-
 	drawState(currentState)
-	drawMove(move)
-	
 	if (arrows) drawMovesArrows(moves)
+	drawMove(move)	
 	
 	//console.log("calculating available moves took", (new Date()) - start, "ms")
 	
@@ -77,13 +73,12 @@ function userMove(move){
 	if (AI){
 		setTimeout(AIMove, 20)
 	}
-	
 }
 
 function AIMove(){
 	if (gameover) return
 	
-	start = new Date();
+	start = new Date() // required for move timing
 	
 	var depth = 0
 	var compMove
@@ -94,34 +89,20 @@ function AIMove(){
 		if (score == Infinity) break
 	}
 	
-	currentState = makeMove(currentState, compMove)
-	
+	currentState = makeMove(currentState, compMove)	
 	currentState.toPlay = currentState.toPlay == "w" ? "b" : "w"
 	moves = availableMoves(currentState, currentState.toPlay)
 
 	appendToLog(compMove, currentState) //matters where this goes as if before state change then code vil change...
-	document.getElementById("info").innerText = "to play:\n" + (currentState.toPlay == "w" ? "white" : "black") + "\n\nthought for:\n" + (new Date() - start).toString() + " ms\n\nthought to depth:\n" + depth + "\n\nwhite eval.:\n" + (Math.round(evaluate(currentState, "w")*10)/10).toString() + "\n\n\n"
-	
+	updateInfo(depth)
 	
 	drawState(currentState)
 	if (arrows) drawMovesArrows(moves)
 	drawMove(compMove)
 
 	checkmateOrStalemate(currentState, moves, currentState.toPlay)
-
-}
-
-function mouse(e){
-	var r = Math.floor(e.offsetY / sqrSize)
-	var c = Math.floor(e.offsetX / sqrSize)
 	
-	drawState(currentState)
-	drawMovesSqrs(moveOptions)
-	
-	ctx.fillStyle = "rgba(20,255,160,0.6)"
-	ctx.beginPath();
-	ctx.arc((c+0.5) * sqrSize, (r+0.5) * sqrSize, sqrSize/2, 0, 2 * Math.PI)
-	ctx.fill()
+	start = new Date() // required for move timing
 }
 
 function click(e){
@@ -146,67 +127,4 @@ function click(e){
 		drawMovesSqrs(moveOptions)
 	}
 	
-}
-
-
-function drawMovesArrows(moves){
-	for (var m = 0; m < moves.length; m++){
-		drawArrow((moves[m][0][1] + 0.5) * sqrSize, (moves[m][0][0] + 0.5) * sqrSize,
-		(moves[m][1][1] + 0.5) * sqrSize, (moves[m][1][0] + 0.5) * sqrSize,	35, 30)
-	}
-}
-
-function drawMove(move){
-	drawArrow((move[0][1] + 0.5) * sqrSize, (move[0][0] + 0.5) * sqrSize, (move[1][1] + 0.5) * sqrSize, (move[1][0] + 0.5) * sqrSize, 35, 30)
-}
-
-
-
-function drawMovesSqrs(moves){
-	for (var m = 0; m < moves.length; m++){
-		ctx.fillStyle = moveColor
-		ctx.fillRect(moves[m][1][1] * sqrSize, moves[m][1][0] * sqrSize, sqrSize, sqrSize)
-	}
-}
-
-
-function drawState(state){
-	//cls
-	ctx.clearRect(0,0,width,width)
-	//drawing wood background
-	drawWoodBackground()
-	//drawing pieces
-	for (var r = 0; r < 8; r++){
-		for (var c = 0; c < 8; c++){
-			if (state.board[r][c] != " "){
-				ctx.drawImage(document.getElementById(state.board[r][c]), c * sqrSize, r * sqrSize, sqrSize, sqrSize)
-			}
-		}
-	}
-}
-
-function drawWoodBackground(){
-	ctx.font = (width/50).toString()+ "px monospace"
-	ctx.textAlign = "start"
-	ctx.fillStyle = "white"
-	ctx.fillRect(0,0,width,width)
-	for (var r = 0; r < 8; r++){
-		for (var c = 0; c < 8; c++){
-			ctx.fillStyle = (r+c) % 2 == 0 ? lightColor : darkColor
-			ctx.fillRect(c*sqrSize, r*sqrSize, sqrSize, sqrSize)
-			if (r == 7){
-				ctx.fillStyle = "black"
-				ctx.fillText(String.fromCharCode(97 + c), c * sqrSize + 5, width - 5)
-			} if (c == 0){
-				ctx.fillStyle = "black"
-				ctx.fillText(8 - r, 5, r * sqrSize + sqrSize + (r == 7 ? -18 : -5))
-			} if (r == 0){
-				ctx.fillStyle = "black"
-				ctx.fillText(String.fromCharCode(97 + c), c * sqrSize + 5, 18)
-			} if (c == 7){
-				ctx.fillStyle = "black"
-				ctx.fillText(8 - r, width - 18, r * sqrSize + sqrSize -5)
-			}
-		}
-	}
 }
